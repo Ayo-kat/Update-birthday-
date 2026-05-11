b9b8e1518aa4e38214a29f94e1722a03b13fbc3d
Ayo-kat <ayokatmodise@gmail.com>
2026-05-11 12:41:51 +0200

    Add valid users and their corresponding images

---
+++ script.js
@@ -0,0 +1,8 @@
+const VALID_USERS = ["alex", "sarah", "mike"];
+const USER_IMAGES = {
+  alex: "assets/images/alex.jpg",
+  sarah: "assets/images/sarah.png",
+  mike: "assets/images/mike.jpg"
+};
+
+// ... rest of the code remains the same
